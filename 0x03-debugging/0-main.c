5f924a8e037145f03d4db87e06fe5de8637af7fd
fndiaye2015 <fodendiaye2@gmail.com>
2022-09-19 22:03:55 +0000

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,7 +11,7 @@ int main(void)
         int i;
 
         i = 0;
-        positive_or_negative(i);
+        printf("%d", positive_or_negative(i));
 
         return (0);
 }
