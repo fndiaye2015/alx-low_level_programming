d4430f5dcd37b9bf3328f253ead7e2ad2c502ec0
fndiaye2015 <fodendiaye2@gmail.com>
2023-05-23 08:41:36 -0700

    add the file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ * Description: This program prints a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
