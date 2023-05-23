1726c61ea04b8b8876817de3555c2bcc7b261849
fndiaye2015 <fodendiaye2@gmail.com>
2023-05-23 07:02:41 -0700

    add the file 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * This program prints 
+ * Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0)
+}
