28190364cd4ecb2f2b66d742a2674d7aef1bbfe6
fndiaye2015 <fodendiaye2@gmail.com>
2023-05-29 18:11:25 -0700

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description - This program prints
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+		{	
+			c++;
+			continue;
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
