b09c18fba60cbf4d4b70b4559b2251d44656b9f4
fndiaye2015 <fodendiaye2@gmail.com>
2023-05-30 03:55:41 -0700

    first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description - This program prints all digits from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
